29c1ae20dde9adc7e0b1e4aa6c80f1d2706fecd4
LewisYoon <trexhoon1212@gmail.com>
2026-07-19 18:31:48 +1000

    adds api key for functional search into .env file

--- src/services/books-services.js
+++ src/services/books-services.js
@@ -1,6 +1,8 @@
+const API_KEY = import.meta.env.VITE_BOOKS_API_KEY;
+
 export const getBooksBySearchTerm = async (term) => {
   const response = await fetch(
-    `https://www.googleapis.com/books/v1/volumes?q=${term}`,
+    `https://www.googleapis.com/books/v1/volumes?q=${term}&key=${API_KEY}`,
   );
 
   if (!response.ok) {
