1b4d47b1ddf983960e3df581486faceedf525305
LewisYoon <trexhoon1212@gmail.com>
2026-07-20 17:13:13 +1000

    adds basic testing for card redering using mock data

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/google-book/",
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./config/test-setup.js",
+  },
 });
