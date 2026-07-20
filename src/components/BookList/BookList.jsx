52039bd6235a4b551a6ec4353c32c75177854350
LewisYoon <trexhoon1212@gmail.com>
2026-07-20 14:32:35 +1000

    adds bonus MVP: Book Modal

--- src/components/BookList/BookList.jsx
+++ src/components/BookList/BookList.jsx
@@ -1,13 +1,25 @@
 import classes from "./BookList.module.scss";
 import BookCard from "../BookCard/BookCard";
+import BookModal from "../BookModal/BookModal";
+import { useState } from "react";
 
 const BookList = ({ books }) => {
+  const [clickedBook, setClickedBook] = useState(null);
   return (
-    <div className={classes.bookList}>
-      {books.map((book) => (
-        <BookCard key={book.id} book={book} />
-      ))}
-    </div>
+    <>
+      <div className={classes.bookList}>
+        {books.map((book) => (
+          <BookCard
+            key={book.id}
+            book={book}
+            onClick={() => setClickedBook(book)}
+          />
+        ))}
+      </div>
+      {clickedBook && (
+        <BookModal book={clickedBook} onClose={() => setClickedBook(null)} />
+      )}
+    </>
   );
 };
 
